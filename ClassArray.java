3d8c39c67fa2e080cdaa6651c70b4ca8d0dcf1e5
gabrielimanasc <gabrielimanasc@gmail.com>
2021-12-14 11:41:14 -0300

    add classes

---
+++ ClassArray.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.Scanner;
+
+public class ClassArray {
+
+    int meuArray[] = new int[ 10 ];
+    Scanner input = new Scanner( System.in );
+
+    public void putNumbers() {
+        System.out.print( "Informe um número: " );
+        meuArray[ i ] = input.nextInt();
+        
+    }
+
+    
+
+}
+
