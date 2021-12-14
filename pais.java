38a147b5403b53307018e934b3dced030df1506b
gabrielimanasc <gabrielimanasc@gmail.com>
2021-12-14 11:50:15 -0300

    adicionando 4 novas classes

---
+++ pais.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.Scanner;
+
+public class ClassArray {
+
+    String olaString;
+    Scanner input = new Scanner( System.in );
+
+    public void putNumbers() {
+        System.out.print( "Informe o nome: " );
+        olaString = input.nextInt();
+        
+    }
+
+    
+
+}
+
